fb2654ae69ca5d07ea2011183679d92c26114b72
François Mockers <mockersf@gmail.com>
2017-11-25 20:46:48 +0100

    test update

--- FantastretchTests/FantastretchTests.swift
+++ FantastretchTests/FantastretchTests.swift
@@ -16,26 +16,26 @@ class FantastretchTests: XCTestCase {
     // Confirm that the Meal initializer returns a Meal object when passed valid parameters.
     func testStretchInitializationSucceeds() {
         // Zero rating
-        let zeroRatingStretch = Stretch(name: "Zero", photo: nil, rating: 0)
+        let zeroRatingStretch = Stretch(name: "Zero", description: "", photo: nil, rating: 0, sides: Side.Center, target: Target.Legs, id: nil)
         XCTAssertNotNil(zeroRatingStretch)
 
         // Highest positive rating
-        let positiveRatingStretch = Stretch(name: "Positive", photo: nil, rating: 5)
+        let positiveRatingStretch = Stretch(name: "Positive", description: "", photo: nil, rating: 5, sides: Side.Center, target: Target.Legs, id: nil)
         XCTAssertNotNil(positiveRatingStretch)
     }
 
     // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
     func testStretchInitializationFails() {
         // Negative rating
-        let negativeRatingStretch = Stretch(name: "Negative", photo: nil, rating: -1)
+        let negativeRatingStretch = Stretch(name: "Negative", description: "", photo: nil, rating: -1, sides: Side.Center, target: Target.Legs, id: nil)
         XCTAssertNil(negativeRatingStretch)
 
         // Empty String
-        let emptyStringStretch = Stretch(name: "", photo: nil, rating: 0)
+        let emptyStringStretch = Stretch(name: "", description: "", photo: nil, rating: 1, sides: Side.Center, target: Target.Legs, id: nil)
         XCTAssertNil(emptyStringStretch)
 
         // Rating exceeds maximum
-        let largeRatingStretch = Stretch(name: "Large", photo: nil, rating: 6)
+        let largeRatingStretch = Stretch(name: "", description: "", photo: nil, rating: 6, sides: Side.Center, target: Target.Legs, id: nil)
         XCTAssertNil(largeRatingStretch)
     }
 }
